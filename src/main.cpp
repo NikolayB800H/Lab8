f44680488d0ec1c96b9da9ae04db5079d88f7ef8
Nikolay Gorkunov <nichto77@gmail.com>
2021-12-29 22:03:30 +0300

    Almost...

--- src/main.cpp
+++ src/main.cpp
@@ -9,38 +9,27 @@
 constexpr size_t PRECISION = 5;
 constexpr size_t NAME_SIZE = 50;
 
-int main() {
-    std::cout << "Running lab 10\n"
-                 "For solving equation A * X = B enter file where is A from:"
-              << std::endl;
-    char A_file_name[NAME_SIZE];
-    std::cin >> A_file_name;
-    std::cout << "Enter file where is B from:" << std::endl;
-    char B_file_name[NAME_SIZE];
-    std::cin >> B_file_name;
-    std::ifstream matrix_A(A_file_name);
-    Matrix A(matrix_A);
-    matrix_A.close();
-    std::cout << "Equation A * X = B:\n" << A << "*\n";
+enum SolveStatus {
+    OK,
+    CANT
+};
+
+static SolveStatus printSolveDSLAE(const Matrix &A, const Matrix &B) {
+    std::cout << "Solving equation A * X = B:\n" << A << "*\n";
     size_t rows = A.getRows();
     size_t cols = A.getCols();
     for (size_t i = 0; i < rows; ++i) {
         std::cout << "  x" << i + 1 << '\n';
     }
     std::cout << "=\n";
-    std::ifstream matrix_B(B_file_name);
-    Matrix B(matrix_B);
-    matrix_B.close();
     std::cout << B
               << "Finding A^-1:\n";
-    A.setPrecision(PRECISION);
-    B.setPrecision(PRECISION);
     Matrix inverted_A(rows, cols);
     try {
         inverted_A = A.invGauJor();
     } catch (SingularMatrix err) {
-        std::cout << err.what() << " - no inverted matrix.\nCan't solve equation." << std::endl;
-        return 0;
+        std::cout << err.what() << " - no inverted matrix." << std::endl;
+        return CANT;
     }
     std::cout << "A^-1 is:\n" << inverted_A;
     Matrix to_test(inverted_A * A);
@@ -49,5 +38,30 @@ int main() {
     testE.fillE();
     std::cout << "Is it equal to E? - " << (to_test == testE)
               << "\nAnswer of the equation:\n" << inverted_A * B;
+    return OK;
+}
+
+int main() {
+    std::cout << "Running lab 10\n"
+                 "For solving equation A * X = B enter file where is A from:"
+              << std::endl;
+    char A_file_name[NAME_SIZE];
+    std::cin >> A_file_name;
+    std::ifstream matrix_A(A_file_name);
+    Matrix A(matrix_A);
+    matrix_A.close();
+    std::cout << "Enter file where is B from:" << std::endl;
+    char B_file_name[NAME_SIZE];
+    std::cin >> B_file_name;
+    std::ifstream matrix_B(B_file_name);
+    Matrix B(matrix_B);
+    matrix_B.close();
+    A.setPrecision(PRECISION);
+    B.setPrecision(PRECISION);
+    switch (printSolveDSLAE(A, B)) {
+        case OK: std::cout << "Equation solved." << std::endl; break;
+        case CANT: std::cout << "Can't solve equation." << std::endl; break;
+        default: std::cout << "Incorrect solve status!" << std::endl;
+    }
     return 0;
 }
